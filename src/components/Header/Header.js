9212f0f4d06d3b19cc8b224fbbca0ce307bf6ced
zee-german <franz.lutzenberger@gmail.com>
2019-03-31 20:00:28 +0200

    simple gallery with images from unsplash

---
+++ src/components/Header/Header.js
@@ -0,0 +1,7 @@
+import './styles.css';
+
+import React from 'react';
+
+const Header = () => <header>What the Splash!?</header>;
+
+export default Header;
---
+++ src/components/ImageGrid/ImageGrid.js
@@ -0,0 +1,44 @@
+import './styles.css';
+
+import React from 'react';
+
+const key = '5f96323678d05ff0c4eb264ef184556868e303b32a2db88ecbf15746e6f25e02';
+
+class ImageGrid extends React.Component {
+  state = {
+    images: []
+  };
+
+  componentDidMount() {
+    fetch(`https://api.unsplash.com/photos/?client_id=${key}&per_page=28`)
+      .then(res => res.json())
+      .then(images => {
+        this.setState({
+          images: images
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  render() {
+    const { images } = this.state;
+    return (
+      <div className="content">
+        <section className="grid">
+          {images.map(image => (
+            <div
+              key={image.id}
+              className={`item item-${Math.ceil(image.height / image.width)}`}
+            >
+              <img src={image.urls.small} alt={image.user.username} />
+            </div>
+          ))}
+        </section>
+      </div>
+    );
+  }
+}
+
+export default ImageGrid;
